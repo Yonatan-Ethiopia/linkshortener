77936ee525225f28c524af8f5131f8161ad6341e
Yonatan-Ethiopia <yonatanethiopia1@gmail.com>
2026-09-23 15:29:46 +0300

    fixed some errors

--- migrations/env.py
+++ migrations/env.py
@@ -31,6 +31,8 @@ target_metadata = SQLModel.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 database_url = os.getenv("DATABASE_URL","")
+if database_url and database_url .startswith("postgres://"):
+    database_url  = database_url .replace("postgres://", "postgresql://", 1)
 config.set_main_option("sqlalchemy.url", database_url)
 
 def run_migrations_offline() -> None:
